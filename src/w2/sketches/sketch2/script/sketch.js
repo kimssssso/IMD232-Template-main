eb5f3dc9005afec47da7ae6bc41ebca2d662fdad
kimssssso <139064060+kimssssso@users.noreply.github.com>
2023-09-18 14:11:31 +0900

    2주차 실습

--- src/w2/sketches/sketch2/script/sketch.js
+++ src/w2/sketches/sketch2/script/sketch.js
@@ -1,12 +1,3 @@
-function setup() {
-  setCanvasContainer('canvas', 960, 640);
-
-  background(255);
-  noStroke();
-  fill('red');
-}
-
-function draw() {
-  background(255);
-  circle(mouseX, mouseY, 50);
-}
+alert('나의 홈페이지에 당도한 것을 환영하오 낯선이여');
+let userTyped = prompt('자네 이름이 무엇인가?', '홍길동');
+confirm('그러니까 너의 이름이' + userTyped + '이라는 거지?');
