b6d8ab7898e815ff43580c8a6322f7cb9ebd4c63
kimssssso <139064060+kimssssso@users.noreply.github.com>
2023-09-25 11:37:43 +0900

    3주차 실습 - 2

--- src/w3/sketches/sketch20/script/sketch.js
+++ src/w3/sketches/sketch20/script/sketch.js
@@ -3,38 +3,6 @@ let vel; // 속도
 let acc; // 가속도
 let rad = 50; // 원의 반지름
 
-class Mover {
-  constructor() {
-    this.pos = createVector(width / 2, height / 2);
-    this.vel = createVector(0, 0);
-    this.acc = createVector();
-  }
-
-  update() {
-    this.acc = p5.Vector.random2D();
-    this.acc.mult(0.5);
-    this.vel.add(this.acc);
-    this.vel.limit(20);
-    this.pos.add(this.vel);
-  }
-
-  infiniteEdge() {
-    if (this.pos.x < 0) {
-      this.pos.x += width;
-    } else if (this.pos.x >= width) {
-      this.pos.x -= width;
-    }
-    if (this.pos.y < 0) {
-      this.pos.y += height;
-    } else if (this.pos.y >= height) {
-      this.pos.y -= height;
-    }
-  }
-
-  display() {
-    ellipse(this.pos.x, this.pos.y, 2 * rad);
-  }
-}
 let anInstance;
 let anotherInstance;
 let instanceArray = [];
